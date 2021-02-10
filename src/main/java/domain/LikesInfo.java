e15eecf59b4e7687f33387ceffb98dedc3c525d2
paulfltv <pavelfltv@yandex.ru>
2021-02-10 16:41:34 +0300

    Commit__file_LikesInfo.java_fixed_and_tag_v2_added

--- src/main/java/domain/LikesInfo.java
+++ src/main/java/domain/LikesInfo.java
@@ -3,6 +3,25 @@ package domain;
 public class LikesInfo {
 	private int likesCount;
 	private boolean canLike;
+	private boolean userLikes;
+	private boolean canPublish;
+
+	public boolean isUserLikes() {
+		return userLikes;
+	}
+
+	public void setUserLikes(boolean userLikes) {
+		this.userLikes = userLikes;
+	}
+
+	public boolean isCanPublish() {
+		return canPublish;
+	}
+
+	public void setCanPublish(boolean canPublish) {
+		this.canPublish = canPublish;
+	}
+
 
 	public boolean isCanLike() {
 		return canLike;
--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -4,11 +4,22 @@ import domain.*;
 
 public class PostManager {
 	private Post[] posts;
-
-	public Post[] search(Copyright copyright, Geo geo, HeaderInfo headerInfo, ActionsInfo actionsInfo, CommentsInfo commentsInfo, LikesInfo likesInfo, RepostsInfo repostsInfo, ViewsInfo viewsInfo) {
+	public Post[] search(Copyright copyright,
+						 Geo geo,
+						 HeaderInfo headerInfo,
+						 ActionsInfo actionsInfo,
+						 CommentsInfo commentsInfo,
+						 LikesInfo likesInfo,
+						 RepostsInfo repostsInfo,
+						 ViewsInfo viewsInfo) {
 		return null;
 	}
-	public void delete (Copyright copyright, Geo geo, HeaderInfo headerInfo, ActionsInfo actionsInfo, CommentsInfo commentsInfo, LikesInfo likesInfo, RepostsInfo repostsInfo, ViewsInfo viewsInfo) {
-		return;
+	public void delete(Copyright copyright,
+					   Geo geo, HeaderInfo headerInfo,
+					   ActionsInfo actionsInfo,
+					   CommentsInfo commentsInfo,
+					   LikesInfo likesInfo,
+					   RepostsInfo repostsInfo,
+					   ViewsInfo viewsInfo) {
 	}
 }

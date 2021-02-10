a16ade5bbe14871f7c6b2d19e2ab0aac88690cdc
paulfltv <pavelfltv@yandex.ru>
2021-02-10 19:13:15 +0300

    Commit_file PostManager.java added

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -3,12 +3,28 @@ package manager;
 import domain.*;
 
 public class PostManager {
+
 	private Post[] posts;
 
-	public Post[] search(Copyright copyright, Geo geo, HeaderInfo headerInfo, ActionsInfo actionsInfo, CommentsInfo commentsInfo, LikesInfo likesInfo, RepostsInfo repostsInfo, ViewsInfo viewsInfo) {
+	public Post[] search(Copyright copyright,
+						 Geo geo,
+						 HeaderInfo headerInfo,
+						 ActionsInfo actionsInfo,
+						 CommentsInfo commentsInfo,
+						 LikesInfo likesInfo,
+						 RepostsInfo repostsInfo,
+						 ViewsInfo viewsInfo) {
 		return null;
 	}
-	public void delete (Copyright copyright, Geo geo, HeaderInfo headerInfo, ActionsInfo actionsInfo, CommentsInfo commentsInfo, LikesInfo likesInfo, RepostsInfo repostsInfo, ViewsInfo viewsInfo) {
+
+	public void delete(Copyright copyright,
+					   Geo geo,
+					   HeaderInfo headerInfo,
+					   ActionsInfo actionsInfo,
+					   CommentsInfo commentsInfo,
+					   LikesInfo likesInfo,
+					   RepostsInfo repostsInfo,
+					   ViewsInfo viewsInfo) {
 		return;
 	}
 }
